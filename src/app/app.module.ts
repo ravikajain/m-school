a466fee3519db17a968c1ea03d0559b740ff3eba
ravika jain <ravika.jain@metacube.com>
2018-02-09 17:58:48 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MetaSchool } from './app.component';
+import { LoginPage } from '../pages/login/login';
+import { TransportPage } from '../pages/transport/transport';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GooglePlus} from '@ionic-native/google-plus';
+
+@NgModule({
+  declarations: [
+    MetaSchool,
+    LoginPage,
+    TransportPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MetaSchool),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MetaSchool,
+    LoginPage,
+    TransportPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    GooglePlus,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {GooglePlus} from '@ionic-native/google-plus';
+import {TransportPage} from '../transport/transport';
+
+// @IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController,public googlePlus: GooglePlus) {
+
+  }
+  login(){
+    console.log("clicked on login");
+    this.googlePlus.login({
+      "webClientId":"796887987566-hu81a9t09vr3i26hdjilgksage0blmo8.apps.googleusercontent.com",
+      "offline":true
+    }).then((res) =>{
+      console.log("loggedin");
+      console.log(res)
+      // this.userServiceProvider.baseURL=this.domainServiceProvider.getBaseURLByEmail(res.email);
+      // this.userServiceProvider.fetchUserData(res)
+      // .then((res)=>{
+      //
+      // }).catch((err)=>{
+      //
+      // });
+      this.navCtrl.setRoot(TransportPage)
+    }).catch((err)=>{
+      console.log(err)
+    });
+    //this.navCtrl.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
+  }
+}
