345d78f70a4233fc7df4e11c831d60529bf7b2ca
ravika jain <ravika.jain@metacube.com>
2018-02-23 11:24:06 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-var connect = require('connect'),
-serveStatic = require('serve-static');
+var express  = require('express');
+var app      = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var app = connect();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(serveStatic("platforms/browser/www"))
-app.listen(process.env.PORT || 5000);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
