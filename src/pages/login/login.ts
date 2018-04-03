0a209b66f3f3bea86c7a9e21b769bd249dd9702b
ravika jain <ravika.jain@metacube.com>
2018-04-03 11:11:46 +0530

    past trips

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,18 +18,18 @@ export class LoginPage {
   }
   login(){
     console.log("clicked on login");
-    if(this.platform._platforms.indexOf("cordova")>-1){
-      this.googlePlus.login({
-        "webClientId":"796887987566-hu81a9t09vr3i26hdjilgksage0blmo8.apps.googleusercontent.com",
-        "offline":true
-      }).then((res) =>{
-        console.log("loggedin");
-        console.log(res)
-      }).catch((err)=>{
-        console.log(err)
-      });
-    } else {
-    }
+    // if(this.platform._platforms.indexOf("cordova")>-1){
+    //   this.googlePlus.login({
+    //     "webClientId":"796887987566-hu81a9t09vr3i26hdjilgksage0blmo8.apps.googleusercontent.com",
+    //     "offline":true
+    //   }).then((res) =>{
+    //     console.log("loggedin");
+    //     console.log(res)
+    //   }).catch((err)=>{
+    //     console.log(err)
+    //   });
+    // } else {
+    // }
     this.navCtrl.setRoot('dashboard')
     //this.navCtrl.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
   }
